3a201cae88ec5b03ad6834b8c1e3e5cf1e2da6c9
Arafin Ahmed <arafin2021@gmail.com>
2023-07-23 01:58:55 +0600

    device log updated

--- app/catalog/urls.py
+++ app/catalog/urls.py
@@ -5,9 +5,13 @@ from . import views
 
 urlpatterns = [
     path("alive/", views.sya_hello),
+    
     path("companies/", views.CompanyList.as_view()),
     path("employees/", views.EmployeeList.as_view()),
     path("devices/", views.DeviceList.as_view()),
+    path("logs/", views.DeviceLogList.as_view()),
+
     path("employees/<int:id>", views.EmployeeDetail.as_view()),
     path("devices/<int:id>", views.DeviceDetail.as_view()),
+    path("logs/<int:id>", views.DeviceLogDetail.as_view()),
 ]
--- app/catalog/views.py
+++ app/catalog/views.py
@@ -3,8 +3,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from .models import Company, Employee, Device
-from .serializers import CompanySerializer, EmployeeSerializer, DeviceSerializer
+from .models import Company, Employee, Device, DeviceLog
+from .serializers import CompanySerializer, EmployeeSerializer, DeviceSerializer, DeviceLogSerializer
 
 from django.http import HttpResponse
 
@@ -54,3 +54,74 @@ class DeviceList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class DeviceLogList(APIView):
+    def post(self, request, format=None):
+        serializer = DeviceLogSerializer(data=request.data)
+        serializer2 = DeviceLogSerializer(data=request.data)
+
+        if serializer2.is_valid() and serializer.is_valid():
+            device_id = serializer.data["device_id"]
+            employee_id = serializer.data["employee_id"]
+            device = Device.objects.get(id=device_id)
+            device_s = DeviceSerializer(device)
+            employee = Employee.objects.get(id=employee_id)
+            employee_s = EmployeeSerializer(employee)
+
+            device_company = device_s.data["company_id"]
+            employee_company = employee_s.data["company_id"]
+
+            if device_company != employee_company:
+                return Response("Device and Employee must be from the same company", status=status.HTTP_400_BAD_REQUEST)
+            
+            try:
+                devicelog = DeviceLog.objects.all().filter(device_id=device_id).latest('id')
+                devicelog_s = DeviceLogSerializer(devicelog)
+                if devicelog_s.data["return_date"] == None:
+                    return Response("Device is already checked out", status=status.HTTP_400_BAD_REQUEST)
+                else:
+                    serializer.save()
+                    return Response(serializer.data, status=status.HTTP_201_CREATED)
+            except:
+                serializer2.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request, format=None):
+        serializer = DeviceLogSerializer(data=request.data)
+        serializer2 = DeviceLogSerializer(data=request.data)
+
+        if serializer.is_valid() and serializer2.is_valid():
+            device_id = serializer.data["device_id"]
+            employee_id = serializer.data["employee_id"]
+            device = Device.objects.get(id=device_id)
+            device_s = DeviceSerializer(device)
+            employee = Employee.objects.get(id=employee_id)
+            employee_s = EmployeeSerializer(employee)
+
+            device_company = device_s.data["company_id"]
+            employee_company = employee_s.data["company_id"]
+
+            if device_company != employee_company:
+                return Response("Device and Employee must be from the same company", status=status.HTTP_400_BAD_REQUEST)
+
+            try:
+                devicelog = DeviceLog.objects.all().filter(device_id=device_id).latest('id')
+                devicelog_s = DeviceLogSerializer(devicelog)
+                if devicelog_s.data["return_date"] == None:
+                    update = DeviceLog.objects.get(id=devicelog_s.data["id"])
+                    update.return_date = serializer.data["return_date"]
+                    update.return_condition = serializer.data["return_condition"]
+                    update.save()
+                    return Response({"status":"Done"}, status=status.HTTP_200_OK)
+                else:
+                    return Response("Device is not checked out", status=status.HTTP_400_BAD_REQUEST)
+            except Exception as e:
+                return Response("Device is not checked out", status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class DeviceLogDetail(APIView):
+    def get(self, request, id):
+        devicelogs = DeviceLog.objects.all().filter(device_id=id)
+        serializer = DeviceLogSerializer(devicelogs, many=True)
+        return Response(serializer.data)
