913bc38978a94efd71f51e5845e9ac1b40968905
Arafin Ahmed <arafin2021@gmail.com>
2023-07-22 19:16:11 +0600

    db models are created

--- app/catalog/admin.py
+++ app/catalog/admin.py
@@ -1,12 +1,36 @@
 from django.contrib import admin
 
-from .models import Company
+from .models import Company, Device, DeviceLog, Employee
 
 
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
-    fields = ('id', 'name',)
-    list_display = ('id', 'name',)
-    list_filter = ('name', )
-    ordering = ('name',)
+    fields = ('id', 'name', 'email')
+    list_display = ('id', 'name', 'email')
+    list_filter = ('name', 'email')
+    ordering = ('name', 'email')
+    readonly_fields = ('id',)
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    fields = ('id', 'name', 'email', 'company_id')
+    list_display = ('id', 'name', 'email', 'company_id')
+    list_filter = ('name', 'email', 'company_id')
+    ordering = ('name', 'email', 'company_id')
+    readonly_fields = ('id',)
+
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    fields = ('id', 'device_type', 'model', 'serial_number', 'company_id')
+    list_display = ('id', 'device_type', 'model', 'serial_number', 'company_id')
+    list_filter = ('device_type', 'model', 'serial_number', 'company_id')
+    ordering = ('device_type', 'model', 'serial_number', 'company_id')
+    readonly_fields = ('id',)
+
+@admin.register(DeviceLog)
+class DeviceLogAdmin(admin.ModelAdmin):
+    fields = ('id', 'device_id', 'employee_id', 'checkout_date', 'return_date', 'checkout_condition', 'return_condition')
+    list_display = ('id', 'device_id', 'employee_id', 'checkout_date', 'return_date', 'checkout_condition', 'return_condition')
+    list_filter = ('device_id', 'employee_id', 'checkout_date', 'return_date', 'checkout_condition', 'return_condition')
+    ordering = ('device_id', 'employee_id', 'checkout_date', 'return_date', 'checkout_condition', 'return_condition')
     readonly_fields = ('id',)
--- app/catalog/models.py
+++ app/catalog/models.py
@@ -1,13 +1,36 @@
-import uuid
-
 from django.db import models
 
-
 class Company(models.Model):
-    id = models.UUIDField(
-        primary_key=True, default=uuid.uuid4, editable=False
-    )
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
 
     def __str__(self):
-        return f'{self.id}'
+        return self.name
+
+class Employee(models.Model):
+    company_id = models.ForeignKey(Company, to_field='id', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Device(models.Model):
+    company_id = models.ForeignKey(Company, to_field='id', on_delete=models.CASCADE)
+    device_type = models.CharField(max_length=50)
+    model = models.CharField(max_length=100)
+    serial_number = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return f"{self.device_type} - {self.model} ({self.serial_number})"
+
+class DeviceLog(models.Model):
+    device_id = models.ForeignKey(Device, to_field='id', on_delete=models.CASCADE)
+    employee_id = models.ForeignKey(Employee, to_field='id', on_delete=models.CASCADE)
+    checkout_date = models.DateTimeField()
+    return_date = models.DateTimeField(null=True, blank=True)
+    checkout_condition = models.TextField()
+    return_condition = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.device_id} - {self.employee_id} ({self.checkout_date})"
