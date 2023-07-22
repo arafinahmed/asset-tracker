6d82e8be01a5537de58c4d5ec8cad202f8ae5cc1
Arafin Ahmed <arafin2021@gmail.com>
2023-07-22 21:21:26 +0600

    Employee api working

--- app/catalog/urls.py
+++ app/catalog/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     path("alive/", views.sya_hello),
     path("companies/", views.CompanyList.as_view()),
+    path("employees/", views.EmployeeDetail.as_view()),
+    path("employees/<int:id>", views.EmployeeList.as_view()),
 ]
--- app/catalog/views.py
+++ app/catalog/views.py
@@ -3,8 +3,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from .models import Company
-from .serializers import CompanySerializer
+from .models import Company, Employee
+from .serializers import CompanySerializer, EmployeeSerializer
 
 from django.http import HttpResponse
 
@@ -22,6 +22,20 @@ class CompanyList(APIView):
 
     def post(self, request, format=None):
         serializer = CompanySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class EmployeeList(APIView):
+    def get(self, request, id):
+        employees = Employee.objects.all().filter(company_id=id)
+        serializer = EmployeeSerializer(employees, many=True)
+        return Response(serializer.data)
+
+class EmployeeDetail(APIView):
+    def post(self, request, format=None):
+        serializer = EmployeeSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
